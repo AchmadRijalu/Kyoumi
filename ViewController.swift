c2b8b9ab7fe86dc09e47683ad7cef893b8462a40
Achmad Rijalu Arianindita <achmad.rijalu@gmail.com>
2024-04-28 20:53:51 +0700

    initial commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kyoumi
+//
+//  Created by Achmad Rijalu on 14/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
